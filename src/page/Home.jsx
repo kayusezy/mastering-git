a16955290edee37b1a11b754244b97ca16e2f76d
kayode <olowookerekayode6@gmail.com>
2025-11-24 10:57:33 +0100

    modified home file

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,7 +2,8 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="text-5xl bg-amber-600 h-screen">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias accusamus deserunt, quas est vero provident officiis ratione esse autem eligendi tenetur non commodi unde temporibus nesciunt, vel totam error iusto.
+    Incidunt, molestias fuga nihil magni aliquam doloribus deserunt voluptatibus nam, suscipit numquam cumque nulla! Cumque quae aliquam eos quos! Quae amet laboriosam odit maiores vel accusamus nobis provident odio quisquam.</div>
   )
 }
 
